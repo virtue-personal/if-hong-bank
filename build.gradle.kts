ae22e9b17587b1cc30c6d4972b549b03ec14932c
악덕 <rudejrqkr97@gmail.com>
2025-04-15 03:31:29 +0900

    refactor: build.gradle.kts 파일 정리
    - 불필요한 스프링 부트 의존성 제거
    - 코틀린 플러그인 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "3.2.3"
     kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "1.8.0"
-
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "org.example"
@@ -14,11 +10,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.2.3")
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
     testImplementation(kotlin("test"))
 }
 
