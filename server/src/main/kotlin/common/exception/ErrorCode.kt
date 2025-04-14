3c07e068f83e26e0c6619bf7932e9448edb2ada9
악덕 <rudejrqkr97@gmail.com>
2025-04-15 03:51:30 +0900

    feat: ErrorCode 클래스에 오류 코드 추가
    - 'PROVIDER_NOT_FOUND' 오류 코드 추가
    - 'TOKEN_IS_INVALID' 오류 코드 추가
    - 'TOKEN_IS_EXPIRED' 오류 코드 추가

--- server/src/main/kotlin/common/exception/ErrorCode.kt
+++ server/src/main/kotlin/common/exception/ErrorCode.kt
@@ -11,5 +11,8 @@ enum class ErrorCode(
 ): CodeInterface {
     AUTH_CONFIG_NOT_FOUNT(-100, "auth config not found"),
     FAILED_TO_CALL_CLIENT(-101, "Failed to call client"),
-    CALL_RESULT_BODY_NULL(-102, "body is null")
+    CALL_RESULT_BODY_NULL(-102, "body is null"),
+    PROVIDER_NOT_FOUND(-103, "provider not found"),
+    TOKEN_IS_INVALID(-104, "token invalid"),
+    TOKEN_IS_EXPIRED(-105, "token expired")
 }
