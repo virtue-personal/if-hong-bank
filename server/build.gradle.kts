41109ea9d0dd53ad1916c8f582caf13d3ea5c4f2
악덕 <rudejrqkr97@gmail.com>
2025-04-15 01:59:26 +0900

    feat: MySQL과 MongoDB 라이브러리 추가
    - build.gradle.kts 파일에 MySQL과 MongoDB를 사용하기 위한 라이브러리를 추가함
    - Spring Boot Data JPA, MySQL Connector, Kotlin Reflect, Kotlin Stdlib JDK8 라이브러리를 MySQL을 위해 추가함
    - Spring Boot Starter Data MongoDB, Kotlin Reflect 라이브러리를 MongoDB를 위해 추가함

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -17,6 +17,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation:3.2.3")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // mysql
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // mongoDB
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.test {
