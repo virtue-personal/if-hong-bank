1c685e73cf2fc66f8f33cc9913e6f4655a18f6f0
악덕 <rudejrqkr97@gmail.com>
2025-04-15 03:51:20 +0900

    feat: AuthService 클래스 추가
    - OAuth 서비스와 JWT 제공자를 이용한 인증 처리를 위한 AuthService 클래스 추가
    - 사용자가 선택한 OAuth 서비스를 이용해 토큰을 얻고 사용자 정보를 가져옴
    - 사용자 정보를 이용해 JWT 토큰 생성

--- server/src/main/kotlin/domains/auth/service/AuthService.kt
+++ server/src/main/kotlin/domains/auth/service/AuthService.kt
@@ -1,1 +1,27 @@
 package org.example.domains.auth.service
+
+import org.example.common.exception.CustomException
+import org.example.common.exception.ErrorCode
+import org.example.common.jwt.JwtProvider
+import org.example.interfaces.OAuthServiceInterface
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(
+    private val oAuthServices: Map<String, OAuthServiceInterface>,
+    private val jwtProvider: JwtProvider
+) {
+    fun handleAuth(state: String, code: String): String {
+        // GOOGLE -> google
+        val provider = state.lowercase()
+
+        val callService = oAuthServices[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
+
+        val accessToken = callService.getToken(code)
+        val userInfo = callService.getUserInfo(accessToken.accessToken)
+        val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
+
+        // userInfo
+    }
+
+}
