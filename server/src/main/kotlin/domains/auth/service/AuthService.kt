4551870f6c6c3b48ce1d83e9c827bbb7f9c9c8cd
악덕 <rudejrqkr97@gmail.com>
2025-04-15 17:15:53 +0900

    refactor: AuthService에 로깅 및 트랜잭션 처리 추가
    - AuthService의 'handleAuth' 메소드에 로깅 기능 추가
    - 'handleAuth' 메소드에 트랜잭션 처리 추가

--- server/src/main/kotlin/domains/auth/service/AuthService.kt
+++ server/src/main/kotlin/domains/auth/service/AuthService.kt
@@ -1,19 +1,26 @@
 package org.example.domains.auth.service
 
+import jakarta.transaction.Transactional
 import org.example.common.exception.CustomException
 import org.example.common.exception.ErrorCode
 import org.example.common.jwt.JwtProvider
+import org.example.common.logging.Logging
 import org.example.interfaces.OAuthServiceInterface
+import org.hibernate.query.sqm.tree.SqmNode.log
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService(
     private val oAuthServices: Map<String, OAuthServiceInterface>,
-    private val jwtProvider: JwtProvider
+    private val jwtProvider: JwtProvider,
+    private val logger: Logger = Logging.getLogger(AuthService::class.java)
 ) {
-    fun handleAuth(state: String, code: String): String {
+    @Transactional
+    fun handleAuth(state: String, code: String): String = Logging.logFor(logger){ log ->
         // GOOGLE -> google
         val provider = state.lowercase()
+        log["provider"] = provider
 
         val callService = oAuthServices[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
 
