b6c5fa51affde641a89ed948f234d48d406486aa
악덕 <rudejrqkr97@gmail.com>
2025-04-15 17:53:15 +0900

    refactor: 로깅 클래스 코드 스타일 개선
    - 로깅 클래스의 코드 스타일을 개선했습니다.
    - 코드의 가독성을 향상시키기 위해 공백과 줄바꿈을 조정했습니다.

--- server/src/main/kotlin/common/logging/Logging.kt
+++ server/src/main/kotlin/common/logging/Logging.kt
@@ -5,10 +5,10 @@ import org.example.common.exception.ErrorCode
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object Logging {
-    fun <T:Any> getLogger(clazz: Class<T>): Logger = LoggerFactory.getLogger(clazz)
+object Logging{
+    fun <T : Any> getLogger(clazz: Class<T>): Logger  = LoggerFactory.getLogger(clazz)
 
-    fun <T> logFor(log: Logger, function: (MutableMap<String, Any>) -> T?): T {
+    fun <T> logFor(log : Logger, function : (MutableMap<String, Any>) -> T?) : T {
         val logInfo = mutableMapOf<String, Any>()
         logInfo["start_at"] = now()
 
@@ -21,8 +21,7 @@ object Logging {
         return result ?: throw CustomException(ErrorCode.FAILED_TO_INVOKE_IN_LOGGER)
     }
 
-    private fun now(): Long {
+    private fun now() : Long {
         return System.currentTimeMillis()
     }
-
 }
